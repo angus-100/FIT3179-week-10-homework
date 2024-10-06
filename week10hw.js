212e9314d81667985abab9c4d23ba8f89e9d100a
angus-100 <aplo0001@student.monash.edu>
2024-10-06 23:19:17 +1100

    Add files via upload

---
+++ week10hw.js
@@ -0,0 +1,9 @@
+var vg_2 = "week9hwAtt2.vg.json";
+vegaEmbed("#shark_map", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "week10hw.vg.json";
+vegaEmbed("#bubble_chart", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
